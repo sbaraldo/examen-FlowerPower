c4efe21fe77021339970b3a127b5a511be4edbb0
sbaraldo <sbaraldo@roc-dev.com>
2022-07-11 11:49:34 +0200

    links gefixt

--- admin/dashnav.php
+++ admin/dashnav.php
@@ -3,7 +3,7 @@
     <div class="logo">
         Admin Panel
     </div> 
-    <li><a href="dashboard.php">Home</a></li>
+    <li><a href="../admin/dashboard.php">Home</a></li>
     <li><a href="../admin/adminproducten.php">Producten</a></li>
     <li><a href="../admin/adminbestellingen.php">Bestellingen</a></li>
     <li><a href="../admin/admingegevens.php">Gegevens</a></li> 
--- medewerker/dashnav.php
+++ medewerker/dashnav.php
@@ -3,7 +3,7 @@
     <div class="logo">
         Medewerker Panel
     </div> 
-    <li><a href="dashboard.php">Home</a></li>
+    <li><a href="../medewerker/dashboard.php">Home</a></li>
     <li><a href="../medewerker/adminproducten.php">Producten</a></li>
     <li><a href="../medewerker/adminbestellingen.php">Bestellingen</a></li>
 </ul>
